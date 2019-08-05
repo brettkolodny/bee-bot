35f1227f90776821a5aecca8e44ea7052410c987
brettkolodny <brettkolodny@gmail.com>
2019-08-05 14:53:14 -0400

    added another image

--- index.js
+++ index.js
@@ -64,7 +64,8 @@ function getWaifu() {
     "https://cdn.imgbin.com/3/25/25/imgbin-queen-bee-monster-musume-everyday-life-with-monster-girls-online-female-lovely-23bX21neha51G0ftqDurtKP3G.jpg",
     "https://vignette.wikia.nocookie.net/monstergirlencyclopedia/images/2/2b/Honey_Bee.jpeg/revision/latest?cb=20150121222651",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQenIP9x0810V7LfK3rft6EWuviK-rpaeTTf7MUVSmbLFta-AoNfw",
-    "https://static.zerochan.net/Nardack.full.283631.jpg"
+    "https://static.zerochan.net/Nardack.full.283631.jpg",
+    "https://cdn.discordapp.com/attachments/507945456064790553/608009488162881586/hqdefault.png"
   ];
 
   return waifus[Math.floor(Math.random() * waifus.length)];
